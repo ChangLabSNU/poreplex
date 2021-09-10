5367a51458f45f15e2f8adae1308b2d8a6990f13
heeseungYoo <yhs252525@gmail.com>
2021-09-10 16:28:50 +0900

    Remove multi-threading.

--- poreplex/pipeline.py
+++ poreplex/pipeline.py
@@ -20,18 +20,12 @@
 # THE SOFTWARE.
 #
 
-import asyncio
-import traceback
-import signal
-import math
+
 import sys
 import os
 from io import StringIO
 from itertools import cycle
 from collections import defaultdict
-from concurrent.futures import (
-    ProcessPoolExecutor, CancelledError, ThreadPoolExecutor)
-from concurrent.futures.process import BrokenProcessPool
 from . import *
 from .io import (
     FASTQWriter, SequencingSummaryWriter, FinalSummaryTracker,
@@ -42,18 +36,6 @@ from .utils import *
 from .fast5_file import get_read_ids
 
 FAST5_SUFFIX = '.fast5'
-
-
-# def force_terminate_executor(executor):
-#     executor._call_queue.empty()
-
-#     if executor._processes:
-#         alive_pids = set(
-#             pid for pid, proc in executor._processes.items()
-#             if proc.is_alive())
-#         for pid in alive_pids:
-#             os.kill(pid, signal.SIGKILL)
-
 
 def scan_dir_recursive_worker(dirname, suffix=FAST5_SUFFIX):
     files, dirs = [], []
@@ -92,24 +74,11 @@ class ProcessingSession:
         self.config = config
         self.logger = logger
 
-        # self.executor_compute = ProcessPoolExecutor(config['parallel'])
-        # self.executor_io = ThreadPoolExecutor(2)
-        # self.executor_mon = ThreadPoolExecutor(2)
-
         self.loop = self.fastq_writer = self.fast5_writer = \
             self.alignment_writer = self.npreaddb_writer = None
         self.dashboard = self.pbar = None
 
     def __enter__(self):
-        # self.loop = asyncio.get_event_loop()
-        # self.executor_compute.__enter__()
-        # self.executor_io.__enter__()
-        # self.executor_mon.__enter__()
-
-        # for signame in 'SIGINT SIGTERM'.split():
-        #     self.loop.add_signal_handler(getattr(signal, signame),
-        #                                  self.stop, signame)
-
         if self.config['fastq_output']:
             self.fastq_writer = FASTQWriter(
                 self.config['outputdir'], self.config['output_layout'])
@@ -125,13 +94,6 @@ class ProcessingSession:
             self.config['barcode_names'])
         self.finalsummary_tracker = FinalSummaryTracker(
             self.config['label_names'], self.config['barcode_names'])
-
-        # if self.config['minimap2_index']:
-        #     self.show_message('==> Loading a minimap2 index file')
-        #     self.alignment_writer = AlignmentWriter(
-        #         self.config['minimap2_index'],
-        #         os.path.join(self.config['outputdir'], 'bam', '{}.bam'),
-        #         self.config['output_layout'])
 
         return self
 
@@ -156,11 +118,6 @@ class ProcessingSession:
             self.alignment_writer.close()
             self.alignment_writer = None
 
-        # self.executor_mon.__exit__(*args)
-        # self.executor_io.__exit__(*args)
-        # self.executor_compute.__exit__(*args)
-        # self.loop.close()
-
     def errx(self, message):
         if self.running:
             errprint(message, end='')
@@ -169,16 +126,6 @@ class ProcessingSession:
     def show_message(self, message):
         if not self.config['quiet']:
             print(message)
-
-    def stop(self, signalname='unknown'):
-        if self.running:
-            if signalname in ['SIGTERM', 'SIGINT']:
-                errprint("\nTermination in process. Please wait for a moment.")
-            self.running = False
-        for task in asyncio.Task.all_tasks():
-            task.cancel()
-
-        self.loop.stop()
 
     def run_in_executor_compute(self, *args):
         return self.loop.run_in_executor(self.executor_compute, *args)
@@ -191,12 +138,6 @@ class ProcessingSession:
 
     def run_process_batch(self, batchid, files):
         # Wait until the input files become ready if needed
-        if self.config['analysis_start_delay'] > 0:
-            try:
-                #await asyncio.sleep(self.config['analysis_start_delay'])
-                pass
-            except CancelledError:
-                return
 
         self.active_batches += 1
         try:
@@ -257,8 +198,6 @@ class ProcessingSession:
                 self.logger.error(stopmsg)
                 self.errx(stopmsg)
 
-        except (CancelledError, BrokenProcessPool):
-            return
         except Exception as exc:
             self.logger.error('Unhandled error during processing reads', exc_info=exc)
             return self.errx('ERROR: Unhandled error ' + str(exc))
@@ -293,8 +232,7 @@ class ProcessingSession:
             del self.jobstack[:]
 
             if reads_to_submit:
-                work = self.run_process_batch(batch_id, reads_to_submit)
-                #self.loop.create_task(work)
+                self.run_process_batch(batch_id, reads_to_submit)
 
     def scan_dir_recursive(self, topdir, dirname=''):
         if not self.running:
@@ -305,9 +243,7 @@ class ProcessingSession:
         try:
             errormsg = None
             dirs, files = scan_dir_recursive_worker(os.path.join(topdir, dirname))
-        except CancelledError as exc:
-            if is_topdir: return
-            else: raise exc
+
         except Exception as exc:
             errormsg = str(exc)
 
@@ -323,7 +259,7 @@ class ProcessingSession:
             for subdir in dirs:
                 subdirpath = os.path.join(dirname, subdir)
                 self.scan_dir_recursive(topdir, subdirpath)
-        except CancelledError as exc:
+        except Exception as exc:
             if is_topdir: return
             else: raise exc
 
@@ -359,224 +295,13 @@ class ProcessingSession:
                         if readpath not in self.reads_done:
                             self.queue_processing(readpath)
 
-        except CancelledError:
+        except Exception:
             pass
-
-    async def wait_until_finish(self):
-        while self.running:
-            try:
-                await asyncio.sleep(0.5)
-            except CancelledError:
-                break
-            
-            ## reads_queued 가 줄지 않음 
-            if self.scan_finished and self.reads_queued <= 0:
-                break
-
-    async def show_progresses_offline(self):
-        from progressbar import ProgressBar, widgets
-
-        barformat_notfinalized = [
-            widgets.AnimatedMarker(), ' ', widgets.Counter(), ' ',
-            widgets.BouncingBar(), ' ', widgets.Timer()]
-
-        class LooseAdaptiveETA(widgets.AdaptiveETA):
-            # Stabilize the ETA on results from large batches rushes in
-            NUM_SAMPLES = 100
-
-        barformat_finalized = [
-            widgets.AnimatedMarker(), ' ', widgets.Percentage(), ' of ',
-            widgets.FormatLabel('%(max)d'), ' ', widgets.Bar(), ' ',
-            widgets.Timer('Elapsed: %s'), ' ', LooseAdaptiveETA()]
-
-        self.pbar = ProgressBar(widgets=barformat_notfinalized)
-        self.pbar.start()
-        notfinalized = True
-
-        while self.running:
-            if notfinalized and self.scan_finished:
-                notfinalized = False
-                self.pbar = ProgressBar(maxval=self.reads_found,
-                                        widgets=barformat_finalized)
-                self.pbar.currval = self.reads_processed
-                self.pbar.start()
-            else:
-                self.pbar.maxval = self.reads_found
-                self.pbar.update(self.reads_processed)
-
-            try:
-                await asyncio.sleep(0.3)
-            except CancelledError:
-                break
-
-    async def show_progresses_live(self):
-        self.show_message('==> Entering LIVE mode.')
-        self.show_message('\nPress Ctrl-C when the sequencing run is finished.')
-        self.show_message('(!) An analysis starts at least {} seconds after the file '
-                          'is discovered.'.format(self.config['analysis_start_delay']))
-        prev_processed = prev_queued = prev_found = -1
-        prev_message_width = 0
-        iterglider = cycle(r'/-\|')
-
-        while self.running:
-            changedany = (
-                prev_processed != self.reads_processed or
-                prev_queued != self.reads_queued or
-                prev_found != self.reads_found)
-
-            if changedany or self.active_batches > 0:
-                msg = "\rLIVE [{}] {} processed, {} queued ({} total reads)".format(
-                        next(iterglider), self.reads_processed, self.reads_queued,
-                        self.reads_found)
-                if len(msg) < prev_message_width:
-                    msg += ' ' * (prev_message_width - len(msg))
-
-                sys.stdout.write(msg)
-                sys.stdout.flush()
-
-                prev_message_width = len(msg)
-                prev_processed = self.reads_processed
-                prev_queued = self.reads_queued
-                prev_found = self.reads_found
-
-            try:
-                await asyncio.sleep(0.3)
-            except CancelledError:
-                break
-
-    async def force_flushing_stalled_queue(self):
-        prev_count = -1
-        heartbeat = max(10, int(self.config['analysis_start_delay'] // 2))
-        stall_counter = 0; stall_trigger = 2
-
-        while self.running:
-            try:
-                await asyncio.sleep(heartbeat)
-            except CancelledError:
-                break
-
-            if self.reads_found != prev_count:
-                stall_counter = 0
-                prev_count = self.reads_found
-                continue
-
-            if self.reads_queued > 0:
-                stall_counter += 1
-
-                if stall_counter >= stall_trigger:
-                    stall_counter = 0
-                    self.flush_jobstack()
-
-    def start_dashboard(self):
-        from . import dashboard
-
-        if self.config['contig_aliases'] and self.config['minimap2_index']:
-            aliases = dashboard.load_aliases(self.config['contig_aliases'])
-        else:
-            aliases = {}
-
-        view = dashboard.DashboardView(self, self.config['barcode_names'],
-                                       'progress', 'mapped_rate',
-                                       self.config['analysis_start_delay'],
-                                       aliases)
-        view.start(self.loop, bool(self.config['minimap2_index']))
-        return view
-
-    def terminate_executors(self):
-        force_terminate_executor(self.executor_compute)
-
-    def finalize_results(self):
-        if self.config['dump_adapter_signals']:
-            self.show_message("==> Creating an inventory for adapter signal dumps")
-            adapter_dump_prefix = os.path.join(self.config['outputdir'], 'adapter-dumps')
-            create_adapter_dumps_inventory(
-                os.path.join(adapter_dump_prefix, 'inventory.h5'),
-                os.path.join(adapter_dump_prefix, 'part-*.h5'))
-
-        if self.config['dump_basecalls']:
-            self.show_message("==> Creating an inventory for basecalled events")
-            events_prefix = os.path.join(self.config['outputdir'], 'events')
-            create_events_inventory(
-                os.path.join(events_prefix, 'inventory.h5'),
-                os.path.join(events_prefix, 'part-*.h5'))
 
     @classmethod
     def run(kls, config, logging):
         with kls(config, logging) as sess:
             sess.show_message("==> Processing FAST5 files")
 
-            if config['live']:
-                # Start monitoring stalled queue
-                mon_task = sess.loop.create_task(sess.force_flushing_stalled_queue())
-            else:
-                # Start monitoring finished processing
-                #mon_task = sess.loop.create_task(sess.wait_until_finish())
-                pass
-
-            # Start a progress updater for the user
-            if config['quiet']:
-                pass
-            elif config['dashboard']:
-                sess.dashboard = sess.start_dashboard()
-            elif config['live']:
-                sess.loop.create_task(sess.show_progresses_live())
-            else:
-                #sess.loop.create_task(sess.show_progresses_offline())
-                pass
-
             # Start the directory scanner
-            scanjob = sess.scan_dir_recursive(config['inputdir'])
-            #sess.loop.create_task(scanjob)
-
-            # Start the directory change watcher in the live mode
-            if config['live']:
-                livewatcher = sess.live_watch_inputs(config['inputdir'])
-                #sess.loop.create_task(livewatcher)
-
-            try:
-                #sess.loop.run_until_complete(mon_task)
-                pass
-            except CancelledError:
-                errprint('\nInterrupted')
-            except Exception as exc:
-                if (isinstance(exc, RuntimeError) and
-                        exc.args[0].startswith('Event loop stopped before Future')):
-                    pass
-                else:
-                    import traceback
-                    errf = StringIO()
-                    traceback.print_exc(file=errf)
-                    errprint('\nERROR: ' + str(exc))
-                    for line in errf.getvalue().splitlines():
-                        logging.error(line)
-
-            #sess.terminate_executors()
-            if sess.dashboard is not None:
-                sess.dashboard.stop()
-
-            # for task in asyncio.Task.all_tasks():
-            #     if not (task.done() or task.cancelled()):
-            #         try:
-            #             try: task.cancel()
-            #             except: pass
-            #             sess.loop.run_until_complete(task)
-            #         except CancelledError:
-            #             errprint('\nInterrupted')
-            #         except Exception as exc:
-            #             if (isinstance(exc, RuntimeError) and
-            #                     exc.args[0].startswith('Event loop stopped before Future')):
-            #                 pass
-            #             else:
-            #                 errprint('\nERROR: ' + str(exc))
-
-            # if not config['quiet'] and sess.scan_finished:
-            #     if sess.pbar is not None:
-            #         sess.pbar.finish()
-            #         sess.show_message('')
-
-            #     if sess.reads_found == sess.reads_processed:
-            #         sess.finalize_results()
-            #         sess.show_message('==> Finished.')
-            #         return sess.finalsummary_tracker.print_results
-            #     else:
-            #         sess.show_message('==> Terminated.')
+            sess.scan_dir_recursive(config['inputdir'])
